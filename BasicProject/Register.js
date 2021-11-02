c6ea5ad9b8ef3d9a41c32fe3417cf85e7af3a71a
Devrath Rana <devrath.rana@kelltontech.com>
2021-11-02 14:53:13 +0530

    fixes

--- BasicProject/Register.js
+++ BasicProject/Register.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
-import { Text, StyleSheet, View, TextInput, Image, Button, Alert } from 'react-native';
+import { Text, StyleSheet, View, TextInput, Image, Alert, SafeAreaView } from 'react-native';
 import { LOGO } from './src/image/index.js';
+import { Button } from 'react-native-elements';
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Separator = () => (
   <View style={styles.separator} />
@@ -8,59 +11,82 @@ const Separator = () => (
 
 export const Register = () => {
   return (
-    <View >
-      <Image
-        style={styles.logo}
-        source={LOGO}
-      />
-      <View style={styles.container}>
-        <TextInput
-          style={ styles.inputBox }
-          placeholder="Full name" />
+    <SafeAreaView style={styles.container}>
+      <View >
+        <Image
+          style={styles.logo}
+          source={LOGO}
+        />
+        <Text style={{ fontSize: 40, fontWeight: 'bold', fontFamily: "serif", color: '#000000', marginLeft: 17 }}>Sign-up</Text>
+        <View>
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Your name" />
           <Separator />
-        <TextInput
-          style={ styles.inputBox }
-          placeholder="Email" />
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.inputBox}
+            placeholder="Your email id" />
           <Separator />
-        <TextInput
-          style={ styles.inputBox }
-          secureTextEntry={true}
-          placeholder="Password"
-        />
-        <Separator />
-        <TextInput
-          style={ styles.inputBox }
-          secureTextEntry={true}
-          placeholder="Confirm password"
-        />
-        <Separator />
-        <Button 
-        title="Create Account" 
-        color="#1b3c42"
-        onPress={() => Alert.alert('Create Account button pressed')}
-        />
-        {/* <Text>Already got an account?</Text>
+          <Text style={styles.label}>Contact no.</Text>
+          <TextInput
+            style={styles.inputBox}
+            secureTextEntry={true}
+            placeholder="Your contact number"
+          />
+          <Separator />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.inputBox}
+            secureTextEntry={true}
+            placeholder="Password"
+          />
+          <Separator />
+          <View style={{marginLeft:20, marginRight:20,}}>
+          <Button
+          ViewComponent={LinearGradient} // Don't forget this!
+          linearGradientProps={{
+            colors: ['red', 'pink'],
+            start: { x: 0, y: 0.5 },
+            end: { x: 1, y: 0.5 },
+          }}
+            title="Create Account"
+          />
+          </View>
+          {/* <Text>Already got an account?</Text>
         <Text style={{color:'#0099ff',backgroundColor:'red', paddingLeft:200,marginBottom:200, }}>Login</Text> */}
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
+
   );
 };
 const styles = StyleSheet.create({
   container: {
-    height: 700,
-    width: 350,
-    paddingTop: 50,
+    flex: 1,
+    backgroundColor: '#ffffff',
+
   },
   logo: {
-    width: 150,
-    height: 100,
-    marginLeft: 112,
-    marginTop: 112,
+    width: 412,
+    height: 210,
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginLeft: 17,
   },
   inputBox: {
-    backgroundColor: '#ffffff', 
+    paddingLeft: 20,
   },
-  separator:{
+  separator: {
     marginVertical: 8,
+    borderBottomColor: '#000000',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginLeft: 20,
+    marginRight: 20,
+
   },
 });
