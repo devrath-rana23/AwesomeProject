4e131d821b5a19bfd266105833d4cd0fc79c234a
Devrath Rana <devrath.rana@kelltontech.com>
2021-11-08 19:04:02 +0530

    fixes

--- BasicProject/Login.js
+++ BasicProject/Login.js
@@ -5,7 +5,12 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 export class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = { passwordIsVisible: true, confirmPasswordIsVisible: true };
+    this.state = {
+      passwordIsVisible: true,
+      confirmPasswordIsVisible: true,
+      email: '',
+      password: '',
+    };
   }
 
   hideShowPasswordFuntionality = () => {
@@ -21,6 +26,49 @@ export class Login extends Component {
   }
 
   formSubmit = () => {
+    let valid_email = false;
+    let valid_password = false;
+
+    if (!this.state.email) {
+      ToastAndroid.showWithGravityAndOffset('Email field is required.',
+        ToastAndroid.LONG,
+        ToastAndroid.TOP,
+        100, 2500
+      );
+    } else {
+      const email_re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (email_re.test(String(this.state.email).toLowerCase())) {
+        valid_email = true;
+      } else {
+        ToastAndroid.showWithGravityAndOffset('Email field is invalid.',
+          ToastAndroid.LONG,
+          ToastAndroid.TOP,
+          100, 2500
+        );
+      }
+    }
+    if (!this.state.password) {
+      ToastAndroid.showWithGravityAndOffset('Password field is required.',
+        ToastAndroid.LONG,
+        ToastAndroid.TOP,
+        100, 2500
+      );
+    } else {
+      const password_regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+      if (password_regex.test(String(this.state.password))) {
+        valid_password = true;
+      } else {
+        ToastAndroid.showWithGravityAndOffset('Password field is invalid.',
+          ToastAndroid.LONG,
+          ToastAndroid.TOP,
+          100, 2500);
+      }
+    }
+    if (valid_email && valid_password) {
+      this.setState((state, props) => {
+        return { submitted: !state.submitted };
+      });
+    }
 
   }
 
@@ -40,6 +88,12 @@ export class Login extends Component {
           <View style={styles.form_fields_container}>
             <Text style={styles.label}>Email</Text>
             <TextInput
+              keyboardType='email-address'
+              onChangeText={(value) => {
+                this.setState((state, props) => {
+                  return { email: value };
+                });
+              }}
               style={styles.inputBox}
               placeholder="Your email id" />
           </View>
@@ -47,6 +101,12 @@ export class Login extends Component {
             <Text style={styles.label}>Password</Text>
             <View style={styles.input_box_with_icons}>
               <TextInput
+                keyboardType='email-address'
+                onChangeText={(value) => {
+                  this.setState((state, props) => {
+                    return { password: value };
+                  });
+                }}
                 style={styles.inputBox}
                 secureTextEntry={this.state.passwordIsVisible}
                 placeholder="Password"
